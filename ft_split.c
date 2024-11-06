7c9ec0e867aa6910da640ed1f7bdb9b0c0dfa4a0
cabraham2 <abraham.clement33@gmail.com>
2024-11-06 15:15:20 +0100

    UPDATE : split
    free tab and return null if one output = NULL

--- ft_split.c
+++ ft_split.c
@@ -6,93 +6,111 @@
 /*   By: clementabraham <clementabraham@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 22:32:41 by cabraham          #+#    #+#             */
-/*   Updated: 2024/11/05 21:54:05 by clementabra      ###   ########.fr       */
+/*   Updated: 2024/11/06 15:12:51 by clementabra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_word(const char *str, char sep)
+// Alloue de la mémoire pour une sous-chaîne de `str` de longueur `len` 
+// à partir de l'index `start` et retourne cette sous-chaîne.
+static char	*malloc_tab(const char *str, int start, int len)
+{
+	char	*sub;
+
+	sub = malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, (char *)str + start, len + 1);
+	return (sub);
+}
+
+// Compte le nombre de mots dans la chaîne `str`, en utilisant `del` 
+// comme délimiteur pour identifier les séparations entre les mots.
+static int	count_words(char const *str, char del)
 {
 	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		while (str[i] == sep)
+		while (str[i] == del)
 			i++;
-		if (str[i] && str[i] != sep)
+		if (str[i] != '\0')
 			count++;
-		while (str[i] && str[i] != sep)
+		while (str[i] != '\0' && str[i] != del)
 			i++;
 	}
 	return (count);
 }
 
-// Duplique une sous-chaîne de longueur n.
-static char	*ft_strdupn(const char *src, int n)
+// Alloue la mémoire pour le tableau principal de chaînes `tab` en fonction 
+//du nombre de mots calculés dans str, ou crée un tableau vide si str est vide.
+static char	**malloc_main_tab(char **tab, char const *str, char del)
 {
-	char	*create;
-	int		i;
-
-	create = malloc((n + 1) * sizeof(char));
-	if (!create)
-		return (NULL);
-	i = 0;
-	while (i < n && src[i])
+	if (str[0] == '\0')
 	{
-		create[i] = src[i];
-		i++;
+		tab = malloc(1 * sizeof(char *));
+		if (!tab)
+			return (NULL);
+		tab[0] = NULL;
+		return (tab);
 	}
-	create[i] = '\0';
-	return (create);
+	tab = malloc(((count_words(str, del) + 1) * sizeof(char *)));
+	if (!tab)
+		return (NULL);
+	return (tab);
 }
 
-// Fonction principale pour diviser la chaîne en fonction du séparateur.
+// Remplit le tableau `tab` avec les mots extraits de `str` en utilisant `del` 
+//comme séparateur ; return 1 si le remplissage est réussi, ifnot 0 en cas d'err
+static int	fill_tab(char const *str, char del, char **tab)
+{
+	int		i;
+	int		n;
+	int		k;
+
+	i = 0;
+	k = 0;
+	while (str[i] != '\0')
+	{
+		n = 0;
+		while (str[i] == del)
+			i++;
+		while (str[i + n] != '\0' && str[i + n] != del)
+			n++;
+		if (n != 0)
+			tab[k++] = malloc_tab(str, i, n);
+		if ((k > 0 && tab[k - 1] == NULL) || !tab)
+			return (0);
+		i = i + n;
+	}
+	tab[k] = NULL;
+	return (1);
+}
+
+// Fonct princip ft_split qui divise la chaîne s en un tableau de chaînes 
+// en fonct du délimiteur c Gère l'allocation et la libération de la mémoire
 char	**ft_split(char const *s, char c)
 {
-	char	**output;
+	char	**tab;
 	int		i;
-	int		j;
-	int		start;
 
 	if (!s)
 		return (NULL);
-	output = malloc((ft_count_word(s, c) + 1) * sizeof(char *));
-	if (!output)
-		return (NULL);
 	i = 0;
-	j = 0;
-	while (s[i])
+	tab = NULL;
+	tab = malloc_main_tab(tab, s, c);
+	if (!tab)
+		return (NULL);
+	if (fill_tab(s, c, tab) == 0)
 	{
-		while (s[i] == c)
-			i++;
-		start = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (i > start)
-			output[j++] = ft_strdupn(s + start, i - start);
+		while (tab[i] != NULL)
+			free(tab[i++]);
+		free(tab);
+		return (NULL);
 	}
-	output[j] = NULL;
-	return (output);
+	return (tab);
 }
-
-/* int main() {
-    char *str = "Hello, world! This is a test string.";
-    char *charset = " ,!.";
-
-    char **result = ft_split(str, charset);
-    int i = 0;
-    
-    while (result[i]) {
-        printf("%s\n", result[i]);
-        free(result[i]);
-        i++;
-    }
-
-    free(result);
-
-    return 0;
-} */
